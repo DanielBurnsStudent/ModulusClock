d8a7169c87e16badcfbf13f55e32ec6e83bf69d7
Daniel <daniel.burns.2022@student.lhschools.org>
2020-02-20 12:41:01 -0500

    userDataCaptured

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,22 @@
 package com.LickingHeights;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	modulusClock();
+    }
+
+    public static void modulusClock(){
+        int hours;
+        int minutes;
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Enter the number of hours.");
+        hours = keyboard.nextInt();
+
+        System.out.println("Enter the number of minutes.");
+        minutes = keyboard.nextInt();
+        
     }
 }
